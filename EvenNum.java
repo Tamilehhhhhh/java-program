6030a8e04ffb20a42960f9f00be37e8f8d54df81
Tamilehhhhhh <tamilsundaram2005@gmail.com>
2025-07-05 10:52:12 +0530

    Update EvenNum.java

--- EvenNum.java
+++ EvenNum.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
-public class EvenNum {
+
+public class PrintEven
+ {
     public static void main(String args[]){
-      Scanner sc=new Scanner(System.in);
-      int n=sc.nextInt()  ;
-      for(int i=0;i<=n;i++){
-        if(i%2==0)
-        System.out.println(i);
-      }
+
+    for(int i=23;i<=56;i++){
+    if(i%2==0){
+        System.out.print(i +" ");
     }
+}
+}
 }
